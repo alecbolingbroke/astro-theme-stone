63b709f0a63c8155ccfb1067be435035483bc3ce
alecbolingbroke <87875824+alecbolingbroke@users.noreply.github.com>
2026-02-22 19:20:59 -0700

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import lottie from "astro-integration-lottie";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(), lottie()],
 });
